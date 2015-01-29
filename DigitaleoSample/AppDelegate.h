36a55ac77e43ca1dd4c6144276b9f93d56f23230
Clément Roulland <croulland@digitaleo.com>
2015-01-29 10:59:50 +0100

    Initial Commit

---
+++ DigitaleoSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DigitaleoSample
+//
+//  Created by Clément Roulland on 29/01/2015.
+//  Copyright (c) 2015 Digitaleo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DigitaleoSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DigitaleoSample
+//
+//  Created by Clément Roulland on 29/01/2015.
+//  Copyright (c) 2015 Digitaleo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
