73cba706d44f5d7adb80a15e08838f2241de0c14
Clément Roulland <croulland@digitaleo.com>
2015-02-05 16:37:45 +0100

    add in-app messaging in NotificationsViewController

--- DigitaleoSample/NotificationsViewController.m
+++ DigitaleoSample/NotificationsViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "NotificationsViewController.h"
+#import <Digitaleo/Digitaleo.h>
 
 @interface NotificationsViewController ()
-
+@property (nonatomic, strong)NSArray *notifications;
 @end
 
 @implementation NotificationsViewController
@@ -17,84 +18,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(handleRefresh:) forControlEvents:UIControlEventValueChanged];
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self loadNotifications];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)handleRefresh:(id)paramSender {
+    [self loadNotifications];
+}
+
+- (void)loadNotifications {
+    [EOMessage findInBackgroundWithBlock:^(NSArray *messages, NSError *error) {
+        if(!error){
+            self.notifications = messages;
+            [self.tableView reloadData];
+        }else{
+            NSLog(@"%s %i | Error on [EOMessage findInBackgroundWithBlock:]: %@", __FUNCTION__, __LINE__, error);
+        }
+        [self.refreshControl endRefreshing];
+    }];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return [self.notifications count];
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"notificationCell" forIndexPath:indexPath];
+    UILabel *label = (UILabel *)[cell viewWithTag:0];
     
-    // Configure the cell...
+    EOMessage *message = [self.notifications objectAtIndex:indexPath.row];
+    [message setStatus:EOMessageStatusDelivered];
+    [message saveStatusInBackground];
+    label.text = [message valueForKey:@"text"];
+    
+    if(message.status < EOMessageStatusRead){
+        [cell setBackgroundColor:[UIColor colorWithRed:0.839 green:0.903 blue:0.992 alpha:1.000]];
+    }else{
+        [cell setBackgroundColor:[UIColor whiteColor]];
+    }
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    EOMessage *message = [self.notifications objectAtIndex:indexPath.row];
+    [message setStatus:EOMessageStatusRead];
+    [message saveStatusInBackground];
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
